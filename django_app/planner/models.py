51888edc80d4e7561668896471f7fda43ef6b1b7
byunghyunpark <openmind8735@gmail.com>
2016-11-01 14:48:25 +0900

    Add null True in planner models

--- django_app/planner/models.py
+++ django_app/planner/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Schedule(models.Model):
     name = models.CharField(max_length=100)
-    plan_start = models.DateTimeField()
-    plan_finish = models.DateTimeField()
+    plan_start = models.DateTimeField(null=True)
+    plan_finish = models.DateTimeField(null=True)
     real_start = models.DateTimeField(null=True)
     real_finish = models.DateTimeField(null=True)
     color = models.CharField(max_length=20)
