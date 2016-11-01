75473784b31d7d7cf94378bea890e8ff4e9b60cf
byunghyunpark <openmind8735@gmail.com>
2016-11-01 15:31:21 +0900

    Add blank True in planner models

--- django_app/planner/models.py
+++ django_app/planner/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Schedule(models.Model):
     name = models.CharField(max_length=100)
-    plan_start = models.DateTimeField(null=True)
-    plan_finish = models.DateTimeField(null=True)
-    real_start = models.DateTimeField(null=True)
-    real_finish = models.DateTimeField(null=True)
+    plan_start = models.DateTimeField(null=True, blank=True)
+    plan_finish = models.DateTimeField(null=True, blank=True)
+    real_start = models.DateTimeField(null=True, blank=True)
+    real_finish = models.DateTimeField(null=True, blank=True)
     color = models.CharField(max_length=20)
 
     def __str__(self):
