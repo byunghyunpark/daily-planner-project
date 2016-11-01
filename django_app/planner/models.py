e756fbf24353109ade8d50f23f9f249d02a81762
byunghyunpark <openmind8735@gmail.com>
2016-11-01 14:30:17 +0900

    Add models in planner app.

--- django_app/planner/models.py
+++ django_app/planner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Schedule(models.Model):
+    name = models.CharField(max_length=100)
+    plan_start = models.DateTimeField()
+    plan_finish = models.DateTimeField()
+    real_start = models.DateTimeField(null=True)
+    real_finish = models.DateTimeField(null=True)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
