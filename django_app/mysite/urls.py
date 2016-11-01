c8327aeca9862f1a7fc8c33caf66367b67ef278c
YongPilMoon <m71661@gmail.com>
2016-11-01 15:29:54 +0900

    Fix url typo

--- django_app/mysite/urls.py
+++ django_app/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('planner.urls', namespace='planner')),
+    url(r'^planner/', include('planner.urls', namespace='planner')),
 
 ]
